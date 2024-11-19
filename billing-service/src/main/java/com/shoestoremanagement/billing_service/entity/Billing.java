d32e1a42c99899c16b8d3442cef4c2442b5fbb87
patrick-bp <papybp@gmail.com>
2024-11-18 21:47:17 -0700

    billing

---
+++ billing-service/src/main/java/com/shoestoremanagement/billing_service/entity/Billing.java
@@ -0,0 +1,46 @@
+package com.shoestoremanagement.billing_service.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.time.Instant;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "billing")
+public class Billing {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, name = "order_number")
+    private Long orderNo;
+    @Column(nullable = false, name = "customer_name")
+    private String customerName;
+    @Column(nullable = false, name = "customer_mobile")
+    private int customerMobile;
+    @Column(nullable = false, name= "order_data")
+    private Timestamp orderDate;
+    @Column(nullable = false, name= "shoe_id")
+    private Long shoeId;
+    @Column(nullable = false, name = "quatity")
+    private int quantity;
+    @Column(nullable = false, name = "unit_price")
+    private BigDecimal unitPrice;
+    @Column(nullable = false, name = "total_price")
+    private BigDecimal total;
+
+    @PrePersist
+    protected void onCreate(){
+        this.orderDate = Timestamp.from(Instant.now());
+    }
+
+
+}
